067d521769d715e184a94211fa6a8b9789270e9b
junki <69guitar1015@gmail.com>
2016-12-14 22:11:54 +0900

    2016/12/14 モータードライバーをEdisonで動かすテスト

--- main.go
+++ main.go
@@ -12,17 +12,27 @@ func main() {
 	gbot := gobot.NewGobot()
 
 	e := edison.NewEdisonAdaptor("edison")
-	led := gpio.NewLedDriver(e, "led", "13")
+	pin1 := gpio.NewDirectPinDriver(e, "pin1", "13")
+	pin2 := gpio.NewDirectPinDriver(e, "pin2", "12")
+
+	state := true
 
 	work := func() {
-		gobot.Every(100*time.Millisecond, func() {
-			led.Toggle()
+		gobot.Every(1000*time.Millisecond, func() {
+			if state {
+				pin2.Off()
+				pin1.On()
+			} else {
+				pin1.Off()
+				pin2.On()
+			}
+			state = !state
 		})
 	}
 
 	robot := gobot.NewRobot("blinkBot",
 		[]gobot.Connection{e},
-		[]gobot.Device{led},
+		[]gobot.Device{pin1, pin2},
 		work,
 	)
 
