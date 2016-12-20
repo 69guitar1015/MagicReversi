d8fea3cb5e7d66a59497d13e6062fe86b63ef3c7
junki <69guitar1015@gmail.com>
2016-12-20 23:13:22 +0900

    2016/12/20 IOエキスパンダの全gpioピンの値を取得できるようになった

--- main.go
+++ main.go
@@ -8,6 +8,53 @@ import (
 	"github.com/hybridgroup/gobot/platforms/intel-iot/edison"
 )
 
+const (
+	// IODIRA is 0x00
+	IODIRA = 0x00 + iota
+	// IODIRB is 0x01
+	IODIRB
+	// IPOLA is 0x02
+	IPOLA
+	// IPOLB is 0x03
+	IPOLB
+	// GPINTENA is 0x04
+	GPINTENA
+	// GPINTENB is 0x05
+	GPINTENB
+	// DEFVALA is 0x06
+	DEFVALA
+	// DEFVALB is 0x07
+	DEFVALB
+	// INTCONA is 0x08
+	INTCONA
+	// INTCONB is 0x09
+	INTCONB
+	// IOCON is 0x0A
+	IOCON
+	// IOCON2 is 0x0B
+	IOCON2
+	// GPPUA is 0x0C
+	GPPUA
+	// GPPUB is 0x0D
+	GPPUB
+	// INTFA is 0x0E
+	INTFA
+	// INTFB is 0x0F
+	INTFB
+	// INTCAPA is 0x10
+	INTCAPA
+	// INTCAPB is 0x11
+	INTCAPB
+	// GPIOA is 0x12
+	GPIOA
+	// GPIOB is 0x13
+	GPIOB
+	// OLATA is 0x14
+	OLATA
+	// OLATB is 0x15
+	OLATB
+)
+
 func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -34,20 +81,22 @@ func main() {
 		defer e.Finalize()
 
 		log.Println("initialize...")
-		err = e.I2cWrite(0x20, []byte{0x00, 0xFF})
+		err = e.I2cWrite(0x20, []byte{IODIRA, 0x00})
 		checkError(err)
-		err = e.I2cWrite(0x20, []byte{0x01, 0xFF})
+		err = e.I2cWrite(0x20, []byte{IODIRB, 0xFF})
+		checkError(err)
+		err = e.I2cWrite(0x20, []byte{IOCON, 0x00})
 		checkError(err)
 
 		log.Println("start reading...")
 		for {
-			err = e.I2cWrite(0x20, []byte{0x12})
+			err = e.I2cWrite(0x20, []byte{GPIOA})
 			checkError(err)
 
 			data, err = e.I2cRead(0x20, 2)
 			checkError(err)
 
-			log.Printf("data : %d と　%d\n", data[0], data[1])
+			log.Printf("data : Aが0b%08b と　Bが0b%08b\n", data[0], data[1])
 
 			time.Sleep(1000 * time.Millisecond)
 		}
