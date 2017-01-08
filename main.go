7d5d142751eb3c862ff91e04ae7f169e50529118
junki <69guitar1015@gmail.com>
2017-01-08 17:20:18 +0900

    main.goの雛形

--- main.go
+++ main.go
@@ -2,22 +2,96 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/69guitar1015/MagicReversi/mrmiddle"
 )
 
-func main() {
-	mm := mrmiddle.NewMrMiddle()
-	mm.Init()
+// this reversi system manages the board status
+// by using only 1, 0, -1 integer as black, none, white
+const (
+	BLACK = 1
+	NONE  = 0
+	WHITE = -1
+)
 
+type state int
+type player int
+type point [2]int
+type board [8][8]state
+
+// reversi middleware interface
+type middleware interface {
+	GetInput() (int, int)
+	Flip(int, int)
+}
+
+// game is main reversi game object
+type game struct {
+	// board object
+	b board
+	// current player
+	crr player
+	// middleware object
+	m middleware
+	// history of putting stone
+	record []point
+}
+
+func newGame(m middleware) (g *game) {
+	g = &game{
+		b: board{
+			[8]state{NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE},
+			[8]state{NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE},
+			[8]state{NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE},
+			[8]state{NONE, NONE, NONE, BLACK, WHITE, NONE, NONE, NONE},
+			[8]state{NONE, NONE, NONE, WHITE, BLACK, NONE, NONE, NONE},
+			[8]state{NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE},
+			[8]state{NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE},
+			[8]state{NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE},
+		},
+		crr: BLACK,
+		m:   m,
+	}
+
+	return
+}
+
+func (g *game) start() {
 	for {
-		ret := mm.ReadWholeBoard()
+		x, y := g.m.GetInput()
 
-		for _, bits := range ret {
-			log.Println(bits)
+		err := g.put(point{x, y})
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		time.Sleep(time.Second)
+		if g.isFinish() {
+			log.Println("Finish!")
+			g.printSummary()
+			break
+		}
 	}
+}
+
+// put a stone to (x, y) address on the board
+func (g *game) put(p point) (err error) {
+	return
+}
+
+// judge whether the game is finished
+func (g *game) isFinish() bool {
+	return true
+}
+
+// print the game summary
+func (g *game) printSummary() {
+
+}
+
+func main() {
+	m := mrmiddle.NewMrMiddle()
+
+	g := newGame(m)
+
+	g.start()
 }
